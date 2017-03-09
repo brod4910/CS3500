5c37c58c3500e2c8091444cc34190cba8c28d49c
Bnagel <Bnagel@SPRING01-12>
2017-03-09 13:18:46 -0700

    Added more test cases

--- Spreadsheet/ControllerTester/ControllerTester.cs
+++ Spreadsheet/ControllerTester/ControllerTester.cs
@@ -32,28 +32,50 @@ namespace ControllerTester
             ISpreadsheetStub stub = new ISpreadsheetStub();
             Controller control = new Controller(stub);
             stub.FireSetContentsOfCell("A1", "1");
-            stub.FireSaveSpreadsheet("test.ss");
-            Assert.IsTrue(stub.hasSavedSpreadsheet);
+            stub.FireSaveSpreadsheet("test1.ss");
+            Assert.AreEqual(stub.Title.ToString(),"test1.ss");
         }
         // TestNewWindow Causes random error
-        /*
-                [TestMethod]
-                public void TestNewWindow()
-                {
-                    ISpreadsheetStub stub = new ISpreadsheetStub();
-                    Controller control = new Controller(stub);
-                    stub.FireSetContentsOfCell("A1", "1");
-                    stub.FireNewWindow();
-                    Assert.IsTrue(stub.hasNewWindow);
-                }
-                */
+        [TestMethod]
+        public void TestNewWindow()
+        {
+            ISpreadsheetStub stub = new ISpreadsheetStub();
+            Controller control = new Controller(stub);
+            stub.FireSetContentsOfCell("A1", "1");
+            stub.FireNewWindow();
+            Assert.IsTrue(stub.hasNewWindow);
+        }
+               
         [TestMethod]
         public void TestOpen()
         {
             ISpreadsheetStub stub = new ISpreadsheetStub();
             Controller control = new Controller(stub);
+            stub.FireSetContentsOfCell("A1", "1");
+            stub.FireSaveSpreadsheet("test.ss");
+            Assert.AreEqual(stub.Title.ToString(), "test.ss");
+            // Opening Spreadsheet Fails
             stub.FireOpenSpreadsheet("test.ss");
             Assert.IsTrue(stub.hasFileOpen);
+        }
+
+        [TestMethod]
+        public void TestSecondConstructor()
+        {
+            ISpreadsheetStub stub = new ISpreadsheetStub();
+            Controller control = new Controller(stub, "test.ss");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.NullReferenceException))]
+        public void TestSaveError()
+        {
+            ISpreadsheetStub stub = new ISpreadsheetStub();
+            Controller control = new Controller(stub);
+            stub.FireSetContentsOfCell("A1", "1");
+            stub.FireSaveSpreadsheet("");
+            // If it never sets the title, then the save threw an error
+            stub.Title.ToString();
         }
 
 
--- Spreadsheet/ControllerTester/ISpreadsheetStub.cs
+++ Spreadsheet/ControllerTester/ISpreadsheetStub.cs
@@ -47,7 +47,11 @@ namespace ControllerTester
         {
             set
             {
-                throw new NotImplementedException();
+                hasTitle = value;
+            }
+            get
+            {
+                return hasTitle;
             }
         }
 
@@ -89,14 +93,15 @@ namespace ControllerTester
         }
 
         public event Action CloseEvent;
-        public event Action<string> FileChosen;
+        public event Func<String, String> FileChosen;
         public event Func<string, string> GetCellContent;
         public event Func<string, string> GetCellValue;
         public event Action NewEvent;
         public event Action NewWindow;
-        public event Action<string> SaveSpreadsheet;
+        public event Func<String, String> SaveSpreadsheet;
         public event Func<string, string, ISet<string>> SetContentsofCell;
         public bool HasClosed;
+        public string hasTitle;
         public bool HasNew;
         public bool hasNewWindow;
         public bool hasSavedSpreadsheet;
@@ -104,33 +109,7 @@ namespace ControllerTester
         private Func<string, string> _savespreadsheet;
         private Func<string, string> _openspreadsheet;
 
-        event Func<string, string> ISpreadsheetView.SaveSpreadsheet
-        {
-            add
-            {
-                _savespreadsheet += value;
-            }
-
-            remove
-            {
-                _savespreadsheet -= value;
-            }
-        }
-
-        event Func<string, string> ISpreadsheetView.FileChosen
-        {
-            add
-            {
-                _openspreadsheet += value;
-            }
-
-            remove
-            {
-                _openspreadsheet -= value;
-            }
-        }
-        
-
+       
         public void DoClose()
         {
             HasClosed = true;
