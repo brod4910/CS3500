1f8fc84448a27adc762c317aa0b1449a9272fefe
Brian Rodriguez <brod4910@gmail.com>
2017-03-18 14:20:57 -0600

    Basic GUI skeleton for Boggle board. No functionality added

--- PS8/BoggleClient/Boggle.Designer.cs
+++ PS8/BoggleClient/Boggle.Designer.cs
@@ -28,20 +28,363 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.OptionsSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.GameTimeButton = new System.Windows.Forms.Button();
+            this.GameTimeTextBox = new System.Windows.Forms.TextBox();
+            this.GameTime = new System.Windows.Forms.Label();
+            this.DomainLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.DomainName = new System.Windows.Forms.TextBox();
+            this.RegisterUserTextBox = new System.Windows.Forms.TextBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.BogglePanel = new System.Windows.Forms.Panel();
+            this.Letter03 = new System.Windows.Forms.Label();
+            this.Letter02 = new System.Windows.Forms.Label();
+            this.Letter01 = new System.Windows.Forms.Label();
+            this.Letter00 = new System.Windows.Forms.Label();
+            this.Letter10 = new System.Windows.Forms.Label();
+            this.Letter11 = new System.Windows.Forms.Label();
+            this.Letter12 = new System.Windows.Forms.Label();
+            this.Letter13 = new System.Windows.Forms.Label();
+            this.Letter30 = new System.Windows.Forms.Label();
+            this.Letter31 = new System.Windows.Forms.Label();
+            this.Letter32 = new System.Windows.Forms.Label();
+            this.Letter33 = new System.Windows.Forms.Label();
+            this.Letter20 = new System.Windows.Forms.Label();
+            this.Letter21 = new System.Windows.Forms.Label();
+            this.Letter22 = new System.Windows.Forms.Label();
+            this.Letter23 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.OptionsSplitContainer)).BeginInit();
+            this.OptionsSplitContainer.Panel1.SuspendLayout();
+            this.OptionsSplitContainer.Panel2.SuspendLayout();
+            this.OptionsSplitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // OptionsSplitContainer
+            // 
+            this.OptionsSplitContainer.Location = new System.Drawing.Point(1, 0);
+            this.OptionsSplitContainer.Name = "OptionsSplitContainer";
+            // 
+            // OptionsSplitContainer.Panel1
+            // 
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.CancelButton);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.GameTimeButton);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.GameTimeTextBox);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.GameTime);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.DomainLabel);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.UsernameLabel);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.DomainName);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.RegisterUserTextBox);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.RegisterButton);
+            this.OptionsSplitContainer.Panel1.Controls.Add(this.BogglePanel);
+            // 
+            // OptionsSplitContainer.Panel2
+            // 
+            this.OptionsSplitContainer.Panel2.Controls.Add(this.textBox1);
+            this.OptionsSplitContainer.Size = new System.Drawing.Size(923, 156);
+            this.OptionsSplitContainer.SplitterDistance = 461;
+            this.OptionsSplitContainer.TabIndex = 0;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(363, 83);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(81, 44);
+            this.CancelButton.TabIndex = 8;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // GameTimeButton
+            // 
+            this.GameTimeButton.Location = new System.Drawing.Point(255, 88);
+            this.GameTimeButton.Name = "GameTimeButton";
+            this.GameTimeButton.Size = new System.Drawing.Size(90, 27);
+            this.GameTimeButton.TabIndex = 7;
+            this.GameTimeButton.Text = "Time";
+            this.GameTimeButton.UseVisualStyleBackColor = true;
+            // 
+            // GameTimeTextBox
+            // 
+            this.GameTimeTextBox.Location = new System.Drawing.Point(140, 89);
+            this.GameTimeTextBox.Name = "GameTimeTextBox";
+            this.GameTimeTextBox.Size = new System.Drawing.Size(100, 26);
+            this.GameTimeTextBox.TabIndex = 6;
+            // 
+            // GameTime
+            // 
+            this.GameTime.AutoSize = true;
+            this.GameTime.Location = new System.Drawing.Point(20, 95);
+            this.GameTime.Name = "GameTime";
+            this.GameTime.Size = new System.Drawing.Size(95, 20);
+            this.GameTime.TabIndex = 5;
+            this.GameTime.Text = "Game Time:";
+            // 
+            // DomainLabel
+            // 
+            this.DomainLabel.AutoSize = true;
+            this.DomainLabel.Location = new System.Drawing.Point(20, 56);
+            this.DomainLabel.Name = "DomainLabel";
+            this.DomainLabel.Size = new System.Drawing.Size(114, 20);
+            this.DomainLabel.TabIndex = 4;
+            this.DomainLabel.Text = "Domain Name:";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(20, 15);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(87, 20);
+            this.UsernameLabel.TabIndex = 3;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // DomainName
+            // 
+            this.DomainName.Location = new System.Drawing.Point(140, 53);
+            this.DomainName.Name = "DomainName";
+            this.DomainName.Size = new System.Drawing.Size(217, 26);
+            this.DomainName.TabIndex = 2;
+            // 
+            // RegisterUserTextBox
+            // 
+            this.RegisterUserTextBox.Location = new System.Drawing.Point(140, 12);
+            this.RegisterUserTextBox.Name = "RegisterUserTextBox";
+            this.RegisterUserTextBox.Size = new System.Drawing.Size(217, 26);
+            this.RegisterUserTextBox.TabIndex = 1;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(363, 31);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(81, 32);
+            this.RegisterButton.TabIndex = 0;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // BogglePanel
+            // 
+            this.BogglePanel.Location = new System.Drawing.Point(11, 162);
+            this.BogglePanel.Name = "BogglePanel";
+            this.BogglePanel.Size = new System.Drawing.Size(901, 436);
+            this.BogglePanel.TabIndex = 1;
+            // 
+            // Letter03
+            // 
+            this.Letter03.AutoSize = true;
+            this.Letter03.Location = new System.Drawing.Point(707, 259);
+            this.Letter03.Name = "Letter03";
+            this.Letter03.Size = new System.Drawing.Size(51, 20);
+            this.Letter03.TabIndex = 0;
+            this.Letter03.Text = "label1";
+            // 
+            // Letter02
+            // 
+            this.Letter02.AutoSize = true;
+            this.Letter02.Location = new System.Drawing.Point(512, 259);
+            this.Letter02.Name = "Letter02";
+            this.Letter02.Size = new System.Drawing.Size(51, 20);
+            this.Letter02.TabIndex = 1;
+            this.Letter02.Text = "label2";
+            // 
+            // Letter01
+            // 
+            this.Letter01.AutoSize = true;
+            this.Letter01.Location = new System.Drawing.Point(317, 259);
+            this.Letter01.Name = "Letter01";
+            this.Letter01.Size = new System.Drawing.Size(51, 20);
+            this.Letter01.TabIndex = 2;
+            this.Letter01.Text = "label3";
+            // 
+            // Letter00
+            // 
+            this.Letter00.AutoSize = true;
+            this.Letter00.Location = new System.Drawing.Point(122, 259);
+            this.Letter00.Name = "Letter00";
+            this.Letter00.Size = new System.Drawing.Size(51, 20);
+            this.Letter00.TabIndex = 3;
+            this.Letter00.Text = "label4";
+            // 
+            // Letter10
+            // 
+            this.Letter10.AutoSize = true;
+            this.Letter10.Location = new System.Drawing.Point(122, 375);
+            this.Letter10.Name = "Letter10";
+            this.Letter10.Size = new System.Drawing.Size(51, 20);
+            this.Letter10.TabIndex = 7;
+            this.Letter10.Text = "label5";
+            // 
+            // Letter11
+            // 
+            this.Letter11.AutoSize = true;
+            this.Letter11.Location = new System.Drawing.Point(317, 375);
+            this.Letter11.Name = "Letter11";
+            this.Letter11.Size = new System.Drawing.Size(51, 20);
+            this.Letter11.TabIndex = 6;
+            this.Letter11.Text = "label6";
+            // 
+            // Letter12
+            // 
+            this.Letter12.AutoSize = true;
+            this.Letter12.Location = new System.Drawing.Point(512, 375);
+            this.Letter12.Name = "Letter12";
+            this.Letter12.Size = new System.Drawing.Size(51, 20);
+            this.Letter12.TabIndex = 5;
+            this.Letter12.Text = "label7";
+            // 
+            // Letter13
+            // 
+            this.Letter13.AutoSize = true;
+            this.Letter13.Location = new System.Drawing.Point(707, 375);
+            this.Letter13.Name = "Letter13";
+            this.Letter13.Size = new System.Drawing.Size(51, 20);
+            this.Letter13.TabIndex = 4;
+            this.Letter13.Text = "label8";
+            // 
+            // Letter30
+            // 
+            this.Letter30.AutoSize = true;
+            this.Letter30.Location = new System.Drawing.Point(122, 585);
+            this.Letter30.Name = "Letter30";
+            this.Letter30.Size = new System.Drawing.Size(51, 20);
+            this.Letter30.TabIndex = 15;
+            this.Letter30.Text = "label9";
+            // 
+            // Letter31
+            // 
+            this.Letter31.AutoSize = true;
+            this.Letter31.Location = new System.Drawing.Point(317, 585);
+            this.Letter31.Name = "Letter31";
+            this.Letter31.Size = new System.Drawing.Size(60, 20);
+            this.Letter31.TabIndex = 14;
+            this.Letter31.Text = "label10";
+            // 
+            // Letter32
+            // 
+            this.Letter32.AutoSize = true;
+            this.Letter32.Location = new System.Drawing.Point(512, 585);
+            this.Letter32.Name = "Letter32";
+            this.Letter32.Size = new System.Drawing.Size(60, 20);
+            this.Letter32.TabIndex = 13;
+            this.Letter32.Text = "label11";
+            // 
+            // Letter33
+            // 
+            this.Letter33.AutoSize = true;
+            this.Letter33.Location = new System.Drawing.Point(707, 585);
+            this.Letter33.Name = "Letter33";
+            this.Letter33.Size = new System.Drawing.Size(60, 20);
+            this.Letter33.TabIndex = 12;
+            this.Letter33.Text = "label12";
+            // 
+            // Letter20
+            // 
+            this.Letter20.AutoSize = true;
+            this.Letter20.Location = new System.Drawing.Point(122, 469);
+            this.Letter20.Name = "Letter20";
+            this.Letter20.Size = new System.Drawing.Size(60, 20);
+            this.Letter20.TabIndex = 11;
+            this.Letter20.Text = "label13";
+            // 
+            // Letter21
+            // 
+            this.Letter21.AutoSize = true;
+            this.Letter21.Location = new System.Drawing.Point(317, 469);
+            this.Letter21.Name = "Letter21";
+            this.Letter21.Size = new System.Drawing.Size(60, 20);
+            this.Letter21.TabIndex = 10;
+            this.Letter21.Text = "label14";
+            // 
+            // Letter22
+            // 
+            this.Letter22.AutoSize = true;
+            this.Letter22.Location = new System.Drawing.Point(512, 469);
+            this.Letter22.Name = "Letter22";
+            this.Letter22.Size = new System.Drawing.Size(60, 20);
+            this.Letter22.TabIndex = 9;
+            this.Letter22.Text = "label15";
+            // 
+            // Letter23
+            // 
+            this.Letter23.AutoSize = true;
+            this.Letter23.Location = new System.Drawing.Point(707, 469);
+            this.Letter23.Name = "Letter23";
+            this.Letter23.Size = new System.Drawing.Size(60, 20);
+            this.Letter23.TabIndex = 8;
+            this.Letter23.Text = "label16";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 26);
+            this.textBox1.TabIndex = 16;
             // 
             // Boggle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 565);
+            this.ClientSize = new System.Drawing.Size(878, 688);
+            this.Controls.Add(this.Letter30);
+            this.Controls.Add(this.Letter31);
+            this.Controls.Add(this.Letter32);
+            this.Controls.Add(this.Letter33);
+            this.Controls.Add(this.Letter20);
+            this.Controls.Add(this.Letter21);
+            this.Controls.Add(this.Letter22);
+            this.Controls.Add(this.Letter23);
+            this.Controls.Add(this.Letter10);
+            this.Controls.Add(this.Letter11);
+            this.Controls.Add(this.Letter12);
+            this.Controls.Add(this.Letter13);
+            this.Controls.Add(this.Letter00);
+            this.Controls.Add(this.Letter01);
+            this.Controls.Add(this.Letter02);
+            this.Controls.Add(this.Letter03);
+            this.Controls.Add(this.OptionsSplitContainer);
             this.Name = "Boggle";
             this.Text = "Boggle";
+            this.OptionsSplitContainer.Panel1.ResumeLayout(false);
+            this.OptionsSplitContainer.Panel1.PerformLayout();
+            this.OptionsSplitContainer.Panel2.ResumeLayout(false);
+            this.OptionsSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OptionsSplitContainer)).EndInit();
+            this.OptionsSplitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer OptionsSplitContainer;
+        private System.Windows.Forms.Label DomainLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.TextBox DomainName;
+        private System.Windows.Forms.TextBox RegisterUserTextBox;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button GameTimeButton;
+        private System.Windows.Forms.TextBox GameTimeTextBox;
+        private System.Windows.Forms.Label GameTime;
+        private System.Windows.Forms.Panel BogglePanel;
+        private System.Windows.Forms.Label Letter03;
+        private System.Windows.Forms.Label Letter02;
+        private System.Windows.Forms.Label Letter01;
+        private System.Windows.Forms.Label Letter00;
+        private System.Windows.Forms.Label Letter10;
+        private System.Windows.Forms.Label Letter11;
+        private System.Windows.Forms.Label Letter12;
+        private System.Windows.Forms.Label Letter13;
+        private System.Windows.Forms.Label Letter30;
+        private System.Windows.Forms.Label Letter31;
+        private System.Windows.Forms.Label Letter32;
+        private System.Windows.Forms.Label Letter33;
+        private System.Windows.Forms.Label Letter20;
+        private System.Windows.Forms.Label Letter21;
+        private System.Windows.Forms.Label Letter22;
+        private System.Windows.Forms.Label Letter23;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- PS8/BoggleClient/IBoggleView.cs
+++ PS8/BoggleClient/IBoggleView.cs
@@ -1,26 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BoggleClient
 {
-    public partial class Boggle : Form, IBoggleView
+    public interface IBoggleView
     {
-        public Boggle()
-        {
-            InitializeComponent();
-        }
-
         /// <summary>
         /// Fired when user must be registered.
         /// Parameters are name and email.
         /// </summary>
-        public event Action<string, string> RegisterPressed;
+        event Action<string, string> RegisterPressed;
     }
 }
